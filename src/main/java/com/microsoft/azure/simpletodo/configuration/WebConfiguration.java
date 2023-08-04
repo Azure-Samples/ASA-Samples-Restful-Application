e78a277b3fd3e3fd49a41b19069fac4f682f1b36
Shili Chen <v-shilichen@microsoft.com>
2023-08-04 08:41:45 +0800

    Support Swagger

--- src/main/java/com/microsoft/azure/simpletodo/configuration/WebConfiguration.java
+++ src/main/java/com/microsoft/azure/simpletodo/configuration/WebConfiguration.java
@@ -1,12 +1,35 @@
 package com.microsoft.azure.simpletodo.configuration;
 
+import com.azure.spring.cloud.autoconfigure.aad.properties.AadAuthenticationProperties;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.OAuthFlow;
+import io.swagger.v3.oas.models.security.OAuthFlows;
+import io.swagger.v3.oas.models.security.Scopes;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
+
 @Configuration
 public class WebConfiguration {
+
+    private final AadAuthenticationProperties properties;
+
+    @Value("${spring-apps.public-url}")
+    private String springAppsPublicUrl;
+
+    public WebConfiguration(AadAuthenticationProperties properties) {
+        this.properties = properties;
+    }
 
     @Bean
     public WebMvcConfigurer webConfigurer() {
@@ -20,5 +43,37 @@ public class WebConfiguration {
                         .allowedHeaders("*");
             }
         };
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/", "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/**");
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        OAuthFlow authorizationCode = new OAuthFlow();
+        authorizationCode.setAuthorizationUrl(String.format("https://login.microsoftonline.com/%s/oauth2/v2.0/authorize/",
+            properties.getProfile().getTenantId()));
+        authorizationCode.setRefreshUrl(String.format("https://login.microsoftonline.com/%s/oauth2/v2.0/token/",
+            properties.getProfile().getTenantId()));
+        authorizationCode.setTokenUrl(String.format("https://login.microsoftonline.com/%s/oauth2/v2.0/token/",
+            properties.getProfile().getTenantId()));
+        authorizationCode.setScopes(new Scopes()
+            .addString(properties.getAppIdUri() + "/ToDo.Read", "Read")
+            .addString(properties.getAppIdUri() + "/ToDo.Write", "Write")
+            .addString(properties.getAppIdUri() + "/ToDo.Delete", "Delete"));
+        OAuthFlows oauthFlows = new OAuthFlows();
+        oauthFlows.authorizationCode(authorizationCode);
+        SecurityScheme securityScheme = new SecurityScheme();
+        securityScheme.setType(SecurityScheme.Type.OAUTH2);
+        securityScheme.setFlows(oauthFlows);
+        return new OpenAPI()
+            .info(new Info()
+                .title("RESTful APIs for SimpleTodo")
+                .version("0.0.1-SNAPSHOT"))
+            .servers(List.of(new Server().url(springAppsPublicUrl)))
+            .components(new Components()
+                .addSecuritySchemes("Azure AD", securityScheme));
     }
 }
